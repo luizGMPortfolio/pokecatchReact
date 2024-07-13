3a6ad7fbe77c262556b866861eb2ce1ac74401a5
Luiz Gustavo Da Silva Moraes <luizgm.dev@gmail.com>
2024-07-13 13:25:33 -0300

    ajustes

--- App/src/components/Game/Inicial.jsx
+++ App/src/components/Game/Inicial.jsx
@@ -5,25 +5,25 @@ import "./Game.css";
 import { useState, useEffect } from "react";
 
 const Inicial = ({setStage, games}) => {
-  const [jogado, setJogado] = useState(false);
 
-  useEffect(() => {
 
-  }, [games])
   return (
     <div className="Inicial">
       <div className="container">
         <ul>
           <li className="Slackey white font-30" onClick={() => setStage('Game1')}>
-            {games.game1 === 'Coletado' && <i class="fa-solid fa-check"></i>}
+            {games.game1 === 'Coletado' && <i class="fa-solid fa-check light-green"></i>}
+            {games.game1 === 'Sem vidas' && <i class="fa-solid fa-x" style={{color: '#db1414'}}></i>}
             <span>Digite o nome</span>
           </li>
           <li className="Slackey white font-30" onClick={() => setStage('Game2')}>
-            {games.game1 === 'Coletado' && <i class="fa-solid fa-check"></i>}
+            {games.game2 === 'Coletado' && <i class="fa-solid fa-check"></i>}
+            {games.game2 === 'Sem vidas' && <i class="fa-solid fa-x" style={{color: '#db1414'}}></i>}
             <span>Acerte o nome</span>
           </li>
           <li className="Slackey white font-30" onClick={() => setStage('Game3')}>
-            {games.game1 === 'Coletado' && <i class="fa-solid fa-check"></i>}
+            {games.game3 === 'Coletado' && <i class="fa-solid fa-check"></i>}
+            {games.game3 === 'Sem vidas' && <i class="fa-solid fa-x" style={{color: '#db1414'}}></i>}
             <span> Acerte pelo tipo</span>
           </li>
           <li className="Slackey white font-30 disable">
